79b78ec67cf5c373ac5d2a41eca03b15187970ee
Roman Guguzin <r-0men@yandex.ru>
2026-06-14 20:51:59 +0300

    redirect

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/polls/', permanent=False)),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls'))
 ] + debug_toolbar_urls()
