ebde5b7858aa04d839d193e2e52daf7416068c62
Roman Guguzin <r-0men@yandex.ru>
2026-06-14 21:20:30 +0300

    checkout

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY= os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY='django-insecure-=)0!mmexlmrl#x+j2t!rk62w9u!8j!k=^4*08m8_*+_kf(au7m'
 DEBUG = os.getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['*']
